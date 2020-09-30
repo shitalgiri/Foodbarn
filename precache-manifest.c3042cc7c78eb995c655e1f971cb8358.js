d923bfb1bfb4d9b1ece2c3e4c0892ee566d4645d
Shital Giri <shitalngiri@gmail.com>
2020-09-30 17:09:03 -0400

    Updates

---
+++ precache-manifest.c3042cc7c78eb995c655e1f971cb8358.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85acd84d8cc3ddfabcfdb4ff95e8bb8d",
+    "url": "/shital-giri-project-5/index.html"
+  },
+  {
+    "revision": "aecaa36e89b585cae8f9",
+    "url": "/shital-giri-project-5/static/css/main.366fe8c2.chunk.css"
+  },
+  {
+    "revision": "159cc5a19170da70387e",
+    "url": "/shital-giri-project-5/static/js/2.278fc572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shital-giri-project-5/static/js/2.278fc572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecaa36e89b585cae8f9",
+    "url": "/shital-giri-project-5/static/js/main.fd1188f2.chunk.js"
+  },
+  {
+    "revision": "6474b941a96e42a8975c",
+    "url": "/shital-giri-project-5/static/js/runtime-main.a0038b8c.js"
+  },
+  {
+    "revision": "b12365231845bf62654b7645b4018536",
+    "url": "/shital-giri-project-5/static/media/headerImg.b1236523.jpg"
+  },
+  {
+    "revision": "2b7c48a767e22546941d70afc9f27ff0",
+    "url": "/shital-giri-project-5/static/media/map.2b7c48a7.svg"
+  },
+  {
+    "revision": "4dd1c615b5ea8ecc76cd476dd111f219",
+    "url": "/shital-giri-project-5/static/media/utensilsImg.4dd1c615.svg"
+  }
+]);
